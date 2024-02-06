338c3f4c284ffbe7dcf8b3f9a4622b77cf90cbce
RachelSimorangkir <143679290+RachelSimorangkir@users.noreply.github.com>
2024-02-06 21:26:59 +0700

    Rachel C.P Simorangkir

--- t03_01.c
+++ t03_01.c
@@ -1,11 +1,34 @@
-// NIM - NAMA
+// 12S23020 - Rachel C.P Simorangkir
 
 #include <stdio.h>
 
-int main(int _argc, char **_argv)
-{
+void min_max_values(int n, int values[], int *min_val, int *max_val) {
+    *min_val = values[0];
+    *max_val = values[0];
+    for (int i = 1; i < n; i++) {
+        if (values[i] < *min_val) {
+            *min_val = values[i];
+        }
+        if (values[i] > *max_val) {
+            *max_val = values[i];
+        }
+    }
+}
 
-  // your code here
+int main() {
+    int n;
+    scanf("%d", &n);
 
-  return 0;
+    int values[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &values[i]);
+    }
+
+    int min_val, max_val;
+    min_max_values(n, values, &min_val, &max_val);
+
+    printf("%d\n", min_val);
+    printf("%d\n", max_val);
+
+    return 0;
 }
